df9e6201ab1e696f9ad1fcd6a0a2216f93181fb2
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2025-12-04 15:57:13 +0100

    feat: - Changed manifest to reduce the area of effect of the extension only to MangaPark. - Fixed issue that did not change servers with two digits - Extended usability by allowing the selection of a server between s00 and s99

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const servers = document.createElement("datalist");
+    servers.id = "servers";
+    let validOptions = [];
+
+    for(let i = 0; i <= 99; i++){
+        let option = document.createElement("option");
+        let optionValue = `s${`${i}`.padStart(2, "0")}`
+        option.value = optionValue;
+        servers.appendChild(option)
+        validOptions.push(optionValue)
+    }
+
+    console.log(servers)
+    serverContainer.appendChild(servers);
+
+    // Just making sure default is not lost
+    serverInput.value = "s01";
+    serverInput.list = servers.id;
+
     const btn = document.getElementById("fixBtn");
     if (!btn) return;
     
     btn.addEventListener("click", () => {
+        const chosenServer = serverInput.value || "s01"; // by default I will keep the original s01 that seemed to work generally
+
+        if (!validOptions.includes(chosenServer))
+        {
+            error.classList.remove("hidden")
+            error.innerText = `Chosen server '${chosenServer}' is not valid (only s00 to s99 allowed)`
+            return;
+        }
+
+        hideError();
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
             chrome.scripting.executeScript({
                 target: { tabId: tabs[0].id },
-                func: fixImagesInPage
+                func: fixImagesInPage,
+                args: [chosenServer]
             });
         });
     });
 });
 
-function fixImagesInPage() {
-    console.log("Fixing images in MangaPark...");
+function fixImagesInPage(server) {
+    console.log("Fixing images in MangaPark with server:", server);
     document.querySelectorAll("img").forEach(img => {
-        if (/https:\/\/s0\d+/.test(img.src)) {
-            img.src = img.src.replace(/s0\d+/, "s01");
+        if (/https:\/\/s[0-9][0-9]/.test(img.src)) {
+            img.src = img.src.replace(/s[0-9][0-9]/, server);
         }
     });
     console.log("Fixed!");
+  }
+
+  function hideError(){
+      error.classList.add("hidden")
+      error.innerText = "";
   }

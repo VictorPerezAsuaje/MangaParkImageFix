6e3ecb6b90db0a3cae53029f36e181d1c18c7fb9
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2026-01-01 10:00:25 +0100

    feat: newer approach for the fix, as the last one lost its validity (server images went down)

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const servers = document.createElement("datalist");
-    servers.id = "servers";
-    let validOptions = [];
+document.addEventListener("DOMContentLoaded", async () => {
+    const loading = document.getElementById("loading");
+    const message = document.getElementById("message");
 
-    for(let i = 0; i <= 99; i++){
-        let option = document.createElement("option");
-        let optionValue = `s${`${i}`.padStart(2, "0")}`
-        option.value = optionValue;
-        servers.appendChild(option)
-        validOptions.push(optionValue)
-    }
+    loading.classList.remove("hidden");
+    message.textContent = "Fixing...";
 
-    console.log(servers)
-    serverContainer.appendChild(servers);
-
-    // Just making sure default is not lost
-    serverInput.value = "s01";
-    serverInput.list = servers.id;
-
-    const btn = document.getElementById("fixBtn");
-    if (!btn) return;
-    
-    btn.addEventListener("click", () => {
-        const chosenServer = serverInput.value || "s01"; // by default I will keep the original s01 that seemed to work generally
-
-        if (!validOptions.includes(chosenServer))
-        {
-            error.classList.remove("hidden")
-            error.innerText = `Chosen server '${chosenServer}' is not valid (only s00 to s99 allowed)`
-            return;
-        }
-
-        hideError();
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: fixImagesInPage,
-                args: [chosenServer]
-            });
+    try{
+        const [tab] = await chrome.tabs.query({ 
+            active: true, 
+            currentWindow: true 
         });
-    });
+
+        await chrome.scripting.executeScript({ 
+            target: { tabId: tab.id }, 
+            func: fixImagesInPage 
+        });
+
+        loading.classList.add("hidden");
+        message.textContent = "Fixed!";
+    }
+    catch (err) { 
+        console.error(err); 
+        loading.classList.remove("hidden"); 
+        message.textContent = "There was an error trying to fix the images"; 
+    }
+    
 });
 
-function fixImagesInPage(server) {
-    console.log("Fixing images in MangaPark with server:", server);
-    document.querySelectorAll("img").forEach(img => {
-        if (/https:\/\/s[0-9][0-9]/.test(img.src)) {
-            img.src = img.src.replace(/s[0-9][0-9]/, server);
-        }
-    });
-    console.log("Fixed!");
-  }
+function fixImagesInPage() {
+    const host = window.location.protocol + "//" + window.location.host;
+    const imgs = document.getElementsByTagName("img");
 
-  function hideError(){
-      error.classList.add("hidden")
-      error.innerText = "";
-  }
+    for (let i = 0; i < imgs.length; i++) {
+        let s = imgs[i].getAttribute("src");
+        if (!s) continue;
+
+        if (s.indexOf("//s") !== -1 && s.indexOf(".") !== -1) {
+            let p = s.split("//")[1];
+            p = p.substring(p.indexOf("/"));
+            imgs[i].src = host + p;
+        }
+    }
+}
